cb5692be02dce61537e3ae422a92811979d11b14
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2015-06-04 17:34:40 +0300

    network

---
+++ hardware/led/network.py
@@ -0,0 +1,38 @@
+import urllib2
+import json
+
+MY_ID = ""
+
+
+def read_json_from_file():
+    with open('base_config.json') as data_file:
+        return json.load(data_file)
+
+
+def first_connect():
+    with open('base_config.json') as data_file:
+        data = json.load(data_file)
+
+    MY_ID = data['id']
+
+    req = urllib2.Request('http://ahome.azure-mobile.net/tables/devices')
+    req.add_header('Content-Type', 'application/json')
+
+    response = urllib2.urlopen(req, json.dumps(data))
+
+
+def get_actions():
+    if MY_ID == '':
+        return None
+    tasks = json.load(urllib2.urlopen("http://ahome.azure-mobile.net/tables/task"))
+    for item in tasks:
+        if item['id'] == MY_ID:
+            return item['actions_list']
+    return None
+
+
+if MY_ID == "":
+    MY_ID = read_json_from_file()['id']
+
+#first_connect()
+print(get_actions())
